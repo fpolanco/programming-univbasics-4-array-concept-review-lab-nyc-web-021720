eb86a094b2fc0a896b325e1060a9a7721b2dbb7f
Franchell Polanco <polanco.franchell@gmail.com>
2020-01-21 17:36:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,8 +20,9 @@ def find_element_index(array,value_to_find)
 end
 
 def find_max_value(array)
-  array.max_by do | element |
-  element.array
+   array.max_by do | element |
+   element.array
+  end
 end
 
 def find_min_value(array)
