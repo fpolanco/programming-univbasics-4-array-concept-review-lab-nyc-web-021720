0720e26cde4a35fe82899f0a61e8d43e4f7be4d5
Franchell Polanco <polanco.franchell@gmail.com>
2020-01-21 17:18:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,13 +6,17 @@
 #   end
 # end
 
-def find_element_index(array, value_to_find)
-  array.length.times do  |count|
-    if array[count] == value_to_find
-      return count
-     end
-   end
-   nil
+# def find_element_index(array, value_to_find)
+#   array.length.times do  |count|
+#     if array[count] == value_to_find
+#       return count
+#      end
+#    end
+#    nil
+# end
+
+def find_element_index(array,value_to_find)
+  array.find_index(value_to_find)
 end
 
 def find_max_value(array)
