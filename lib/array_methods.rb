48480bb7895fa4a0fe19fafba79704ac9c925969
Franchell Polanco <polanco.franchell@gmail.com>
2020-01-21 17:09:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,17 @@
+# def find_element_index(array, value_to_find)
+#   counter = 0
+#   while counter < array.length do
+#     return array.index(value_to_find)
+#     counter += 1
+#   end
+# end
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times do  |count|
+    if array[count] == value_to_find
+      return count
+     end
+     nil
 end
 
 def find_max_value(array)
